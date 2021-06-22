4b97d6ecfa5b9ed9c8fc5875e7120ea5299d7a8e
Potlapalli <potlapalliananth@gmail.com>
2021-06-22 01:39:33 -0500

    Add files via upload

---
+++ StockInvestments.API/Repositories/CurrentPositionsRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StockInvestments.API.DbContexts;
+using StockInvestments.API.Entities;
+
+namespace StockInvestments.API.Repositories
+{
+    public class CurrentPositionsRepository : ICurrentPositionsRepository
+    {
+        private readonly StockInvestmentsContext _stockInvestmentsContext;
+
+        public CurrentPositionsRepository(StockInvestmentsContext context)
+        {
+            _stockInvestmentsContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<CurrentPosition> GetCurrentPositions()
+        {
+            return _stockInvestmentsContext.CurrentPositions.ToList();
+        }
+
+        public CurrentPosition GetCurrentPosition(string ticker)
+        {
+            return _stockInvestmentsContext.CurrentPositions.FirstOrDefault(cp => cp.Ticker == ticker);
+        }
+
+        public void Add(CurrentPosition currentPosition)
+        {
+            _stockInvestmentsContext.CurrentPositions.Add(currentPosition);
+            _stockInvestmentsContext.SaveChanges();
+        }
+
+        public void Update(CurrentPosition dbCurrentPosition, CurrentPosition currentPosition)
+        {
+            dbCurrentPosition.Company = currentPosition.Company;
+            dbCurrentPosition.PurchasePrice = currentPosition.PurchasePrice;
+            dbCurrentPosition.TotalShares = currentPosition.TotalShares;
+
+            _stockInvestmentsContext.SaveChanges();
+        }
+
+        public void Delete(CurrentPosition currentPosition)
+        {
+            _stockInvestmentsContext.CurrentPositions.Remove(currentPosition);
+            _stockInvestmentsContext.SaveChanges();
+        }
+    }
+}
---
+++ StockInvestments.API/Repositories/ICurrentPositionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StockInvestments.API.Entities;
+
+namespace StockInvestments.API.Repositories
+{
+    public interface ICurrentPositionsRepository
+    {
+        IEnumerable<CurrentPosition> GetCurrentPositions();
+        CurrentPosition GetCurrentPosition(string ticker);
+        void Add(CurrentPosition currentPosition);
+        void Update(CurrentPosition dbCurrentPosition, CurrentPosition currentPosition);
+        void Delete(CurrentPosition currentPosition);
+    }
+}
