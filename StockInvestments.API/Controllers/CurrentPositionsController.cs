366a6c75eefe8c8853e7cb43c782fde4cd52d1a2
Ananth Rao <potlapalliananth@gmail.com>
2021-08-09 20:24:03 -0500

    Add files via upload

--- StockInvestments.API/Controllers/CurrentPositionsController.cs
+++ StockInvestments.API/Controllers/CurrentPositionsController.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using StockInvestments.API.Contracts;
 using StockInvestments.API.Entities;
 using StockInvestments.API.Helpers;
 using StockInvestments.API.Models;
-using StockInvestments.API.Services;
 
 namespace StockInvestments.API.Controllers
 {
@@ -19,21 +24,41 @@ namespace StockInvestments.API.Controllers
     /// </summary>
     [Route("api/currentPositions")]
     [ApiController]
+    [HttpCacheExpiration(CacheLocation = CacheLocation.Public, MaxAge = 120)]
+    [HttpCacheValidation(MustRevalidate = true)]
+    //[ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
     public class CurrentPositionsController : ControllerBase
     {
         private readonly ICurrentPositionsRepository _currentPositionsRepository;
         private readonly IMapper _mapper;
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="currentPositionsRepository"></param>
-        /// <param name="mapper"></param>
-        public CurrentPositionsController(ICurrentPositionsRepository currentPositionsRepository, IMapper mapper)
+        private readonly ILogger<CurrentPositionsController> _logger;
+        private readonly IPropertyCheckerService _propertyCheckerService;
+        private readonly ILoggerRepo _loggerRepo;
+
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="currentPositionsRepository"></param>
+       /// <param name="mapper"></param>
+       /// <param name="logger"></param>
+       /// <param name="loggerRepo"></param>
+       /// <param name="propertyCheckerService"></param>
+        public CurrentPositionsController(ICurrentPositionsRepository currentPositionsRepository, IMapper mapper, 
+            ILogger<CurrentPositionsController> logger , ILoggerRepo loggerRepo, IPropertyCheckerService propertyCheckerService)
         {
             _currentPositionsRepository = currentPositionsRepository ??
                                           throw new ArgumentNullException(nameof(currentPositionsRepository));
             _mapper = mapper ?? 
                       throw new ArgumentNullException(nameof(mapper));
+
+            _logger =  logger ??
+                       throw new ArgumentNullException(nameof(logger));
+
+            _loggerRepo = loggerRepo ??
+                          throw new ArgumentNullException(nameof(loggerRepo));
+
+            _propertyCheckerService = propertyCheckerService ??
+                                      throw new ArgumentNullException(nameof(propertyCheckerService)); ;
         }
 
         /// <summary>
@@ -43,7 +68,7 @@ namespace StockInvestments.API.Controllers
         /// <response code="200">Returns CurrentPositions list</response>
         /// 
         //Get api/currentPositions
-        [HttpGet]
+        [HttpGet( Name = "GetCurrentPositions")]
         // [HttpHead]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<CurrentPositionDto>> GetCurrentPositions()
@@ -72,25 +97,55 @@ namespace StockInvestments.API.Controllers
         /// GetCurrentPosition
         /// </summary>
         /// <param name="ticker"></param>
+        /// <param name="fields"></param>
         /// <returns>CurrentPosition</returns>
         /// <response code="200">Returns the specific current position</response>
         /// <response code="400">If the ticker is invalid</response>
         /// <response code="404">If the Current Position couldn't be found</response>
         //Get api/currentPositions/xxx
+        //Get api/currentPositions/xxx?fields=ticker,company
         [HttpGet("{ticker}", Name = "GetCurrentPosition")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<CurrentPositionDto> GetCurrentPosition(string ticker)
+        //[ResponseCache(Duration = 120)]
+        public ActionResult<CurrentPositionDto> GetCurrentPosition(string ticker, string fields, 
+            [FromHeader(Name = "Accept")] string mediaType)
         {
             if (string.IsNullOrEmpty(ticker))
                 return BadRequest("Invalid ticker");
+
+            if (!MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue parsedMediaType))
+            {
+                return BadRequest();
+            }
+
+            if (!_propertyCheckerService.TypeHasProperties<CurrentPositionDto>(fields))
+            {
+                return BadRequest();
+            }
 
             var currentPositionFromRepo = _currentPositionsRepository.GetCurrentPosition(ticker);
             if (currentPositionFromRepo == null)
                 return NotFound("Current Position couldn't be found.");
 
-            return Ok(_mapper.Map<CurrentPositionDto>(currentPositionFromRepo));
+
+            _loggerRepo.LogDebug("Get is Successful");
+
+            if (parsedMediaType.MediaType == "application/vnd.marvin.hateoas+json")
+            {
+                var links = CreateLinksForCurrentPosition(ticker, fields);
+
+                var linkedResourceToReturn = _mapper.Map<CurrentPositionDto>(currentPositionFromRepo).ShapeData(fields)
+                    as IDictionary<string, object>;
+
+                linkedResourceToReturn.Add("links", links);
+
+                return Ok(linkedResourceToReturn);
+            }
+
+            return Ok(_mapper.Map<CurrentPositionDto>(currentPositionFromRepo).ShapeData(fields));
+
         }
 
         /// <summary>
@@ -128,7 +183,7 @@ namespace StockInvestments.API.Controllers
         /// <response code="201">Returns the newly created CurrentPosition</response>
         /// <response code="400">If the CurrentPosition is null</response>
         //Post api/currentPositions
-        [HttpPost]
+        [HttpPost(Name = "CreateCurrentPosition")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<CurrentPositionDto> CreateCurrentPosition(CurrentPositionForCreationDto currentPosition)
@@ -138,8 +193,16 @@ namespace StockInvestments.API.Controllers
             _currentPositionsRepository.Save();
 
             var currentPositionToReturn = _mapper.Map<CurrentPositionDto>(currentPositionEntity);
-            return CreatedAtRoute("GetCurrentPosition", new {ticker = currentPositionToReturn.Ticker},
-                currentPositionToReturn);
+
+            var links = CreateLinksForCurrentPosition(currentPositionToReturn.Ticker, null);
+
+            var linkedResourceToReturn = currentPositionToReturn.ShapeData(null)
+                as IDictionary<string, object>;
+
+             linkedResourceToReturn.Add("links", links);
+
+            return CreatedAtRoute("GetCurrentPosition", new {ticker = linkedResourceToReturn["Ticker"]},
+                linkedResourceToReturn);
         }
 
         /// <summary>
@@ -197,7 +260,7 @@ namespace StockInvestments.API.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult UpdateCurrentPosition(string ticker, CurrentPositionForUpdateDto currentPosition)
+        public IActionResult UpdateCurrentPosition(string ticker, CurrentPositionForUpdateDto currentPosition)
         {
             if (string.IsNullOrEmpty(ticker))
                 return BadRequest("Invalid ticker");
@@ -215,6 +278,46 @@ namespace StockInvestments.API.Controllers
         }
 
         /// <summary>
+        /// PartiallyUpdateCurrentPosition
+        /// </summary>
+        /// <param name="ticker"></param>
+        /// <param name="patchDocument"></param>
+        /// <returns></returns>
+        /// <response code="204">If the current position is updated</response>
+        /// <response code="400">If the ticker are invalid</response>
+        /// <response code="404">If the Current Positions are not found</response>
+        //Patch api/currentPositions/xxx
+        [HttpPatch("{ticker}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult PartiallyUpdateCurrentPosition(string ticker, JsonPatchDocument<CurrentPositionForUpdateDto> patchDocument)
+        {
+            if (string.IsNullOrEmpty(ticker))
+                return BadRequest("Invalid ticker");
+
+            var currentPositionFromRepo = _currentPositionsRepository.GetCurrentPosition(ticker);
+            if (currentPositionFromRepo == null)
+                return NotFound("Current Position couldn't be found.");
+
+            var currentPositionToPatch = _mapper.Map<CurrentPositionForUpdateDto>(currentPositionFromRepo);
+            patchDocument.ApplyTo(currentPositionToPatch , ModelState);
+
+            if (!TryValidateModel(currentPositionToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(currentPositionToPatch, currentPositionFromRepo);
+
+            _currentPositionsRepository.Update(currentPositionFromRepo);
+            _currentPositionsRepository.Save();
+
+            return NoContent();
+        }
+
+
+        /// <summary>
         /// DeleteCurrentPosition
         /// </summary>
         /// <param name="ticker"></param>
@@ -223,7 +326,7 @@ namespace StockInvestments.API.Controllers
         /// <response code="400">If the ticker are invalid</response>
         /// <response code="404">If the Current Positions are not found</response>
         //Delete api/currentPositions/xxx
-        [HttpDelete("{ticker}")]
+        [HttpDelete("{ticker}", Name = "DeleteCurrentPosition")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -240,6 +343,56 @@ namespace StockInvestments.API.Controllers
             _currentPositionsRepository.Save();
 
             return NoContent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelStateDictionary"></param>
+        /// <returns></returns>
+        public override ActionResult ValidationProblem([ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
+        {
+            var options = HttpContext.RequestServices.GetRequiredService<IOptions<ApiBehaviorOptions>>();
+            return (ActionResult)options.Value.InvalidModelStateResponseFactory(ControllerContext);
+        }
+
+        private IEnumerable<LinkDto> CreateLinksForCurrentPosition(string ticker, string fields)
+        {
+            var links = new List<LinkDto>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                links.Add( 
+                    new LinkDto(Url.Link("GetCurrentPosition", new {ticker}),
+                        "self",
+                        "GET"));
+            }
+            else
+            {
+                links.Add(
+                    new LinkDto(Url.Link("GetCurrentPosition", new { ticker, fields }),
+                        "self",
+                        "GET"));
+            }
+
+
+            links.Add(
+                new LinkDto(Url.Link("DeleteCurrentPosition", new { ticker }),
+                    "delete_currentPosition",
+                    "DELETE"));
+
+            links.Add(
+                new LinkDto(Url.Link("CreateSoldPosition", new { ticker }),
+                    "create_currentPosition",
+                    "POST"));
+
+            links.Add(
+                new LinkDto(Url.Link("GetSoldPositionsForCurrentPosition", new { ticker }),
+                    "soldPositions",
+                    "GET"));
+
+
+            return links;
         }
     }
 }
--- StockInvestments.API/Controllers/SoldPositionsController.cs
+++ StockInvestments.API/Controllers/SoldPositionsController.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using StockInvestments.API.Contracts;
 using StockInvestments.API.Entities;
 using StockInvestments.API.Models;
@@ -17,6 +22,7 @@ namespace StockInvestments.API.Controllers
     /// </summary>
     [Route("api/currentPositions/{ticker}/soldPositions")]
     [ApiController]
+    //[ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
     public class SoldPositionsController : ControllerBase
     {
         private readonly ICurrentPositionsRepository _currentPositionsRepository;
@@ -51,9 +57,9 @@ namespace StockInvestments.API.Controllers
         /// <param name="ticker"></param>
         /// <returns>SoldPositionsForCurrentPosition</returns>
         /// <response code="200">Returns SoldPositions list</response>
-        /// <response code="404">If the Current Position couldn't be found</response>
+        /// <response code="404">If the current position couldn't be found</response>
         //Get api/currentPositions/xxx/soldPositions
-        [HttpGet]
+        [HttpGet(Name = "GetSoldPositionsForCurrentPosition")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<SoldPositionDto>> GetSoldPositionsForCurrentPosition(string ticker)
@@ -173,14 +179,12 @@ namespace StockInvestments.API.Controllers
         /// <response code="201">Returns the newly added sold position</response>
         /// <response code="404">If the current position is not found</response>
         /// <response code="204">If the Current Position is deleted</response>
-        /// <response code="400">If the Total number of shares be sold is more than total shares currently available</response>
         //Post api/currentPositions/xxx/soldPositions
-        [HttpPost]
+        [HttpPost(Name = "CreateSoldPosition")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<SoldPositionDto> CreateSoldPosition(string ticker, SoldPositionForCreationDto soldPosition)
+        public IActionResult CreateSoldPosition(string ticker, SoldPositionForCreationDto soldPosition)
         {
             if (!_currentPositionsRepository.CurrentPositionExists(ticker))
             {
@@ -189,36 +193,12 @@ namespace StockInvestments.API.Controllers
 
             var soldPositionEntity = _mapper.Map<SoldPosition>(soldPosition);
 
-            var currentPosition = _currentPositionsRepository.GetCurrentPosition(ticker);
-
-            var sharesRemaining = _soldPositionsRepository.GetSharesRemaining(currentPosition);
-
-            if (sharesRemaining + soldPositionEntity.TotalShares > currentPosition.TotalShares)
-            {
-                return BadRequest($"Total number of shares be sold is more than total shares currently available");
-            }
-
-            if (sharesRemaining + soldPositionEntity.TotalShares == currentPosition.TotalShares)
-            {
-                var totalAmount= _soldPositionsRepository.GetSoldPositionsTotalAmount(ticker);
-                var closedPosition = new ClosedPosition()
-                {
-                    Ticker = ticker,
-                    Company = currentPosition.Company,
-                    FinalValue = (soldPositionEntity.TotalAmount + totalAmount)/ currentPosition.TotalShares - currentPosition.TotalAmount
-
-                };
-                _closedPositionsRepository.Add(closedPosition);
-                _soldPositionsRepository.Save();
-                _currentPositionsRepository.Delete(currentPosition);
-                _soldPositionsRepository.Save();
-
-                return NoContent();
-            }
-
             _soldPositionsRepository.Add(ticker, soldPositionEntity);
             _soldPositionsRepository.Save();
 
+            if (AddClosedPositionBasedOnTotalShares(ticker, soldPositionEntity)) 
+                return NoContent();
+            
             var soldPositionToReturn = _mapper.Map<SoldPositionDto>(soldPositionEntity);
             return CreatedAtRoute("GetSoldPositionForCurrentPosition",
                 new {ticker = ticker, number = soldPositionToReturn.Number},
@@ -234,11 +214,13 @@ namespace StockInvestments.API.Controllers
         /// <returns></returns>
         /// <response code="204">If the sold position is updated</response>
         /// <response code="404">If the Current Position or sold position couldn't be found</response>
+        /// <response code="201">If the sold position is created</response>
         //Put api/currentPositions/xxx/soldPositions/1
         [HttpPut("{number}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult UpdateSoldPositionForCurrentPosition(string ticker, long number, SoldPositionForUpdateDto soldPosition)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult UpdateSoldPositionForCurrentPosition(string ticker, long number, SoldPositionForUpdateDto soldPosition)
         {
             if (!_currentPositionsRepository.CurrentPositionExists(ticker))
             {
@@ -247,36 +229,101 @@ namespace StockInvestments.API.Controllers
 
             var soldPositionFromRepo = _soldPositionsRepository.GetSoldPosition(ticker, number);
             if (soldPositionFromRepo == null)
-                return NotFound("Sold Position couldn't be found.");
+            {
+                var soldPositionEntity = _mapper.Map<SoldPosition>(soldPosition);
+                soldPositionEntity.Number = number;
+
+                _soldPositionsRepository.Add(ticker, soldPositionEntity);
+                _soldPositionsRepository.Save();
+
+                if (AddClosedPositionBasedOnTotalShares(ticker, soldPositionEntity))
+                    return NoContent();
+
+                var soldPositionToReturn = _mapper.Map<SoldPositionDto>(soldPositionEntity);
+                return CreatedAtRoute("GetSoldPositionForCurrentPosition",
+                    new { ticker = ticker, number = soldPositionToReturn.Number },
+                    soldPositionToReturn);
+            }
 
             _mapper.Map(soldPosition, soldPositionFromRepo);
 
             _soldPositionsRepository.Update(soldPositionFromRepo);
             _soldPositionsRepository.Save();
 
-            var currentPosition = _currentPositionsRepository.GetCurrentPosition(ticker);
+            if (AddClosedPositionBasedOnTotalShares(ticker, soldPositionFromRepo))
+                return NoContent();
 
-            var sharesRemaining = _soldPositionsRepository.GetSharesRemaining(currentPosition);
-      
-            if (sharesRemaining  == 0)
-            {
-                var totalAmount = _soldPositionsRepository.GetSoldPositionsTotalAmount(ticker);
-                var closedPosition = new ClosedPosition()
-                {
-                    Ticker = ticker,
-                    Company = currentPosition.Company,
-                    FinalValue = (totalAmount) / currentPosition.TotalShares - currentPosition.TotalAmount
-
-                };
-                _closedPositionsRepository.Add(closedPosition);
-                _soldPositionsRepository.Save();
-                _currentPositionsRepository.Delete(currentPosition);
-                _soldPositionsRepository.Save();
-            }
-        
             return NoContent();
         }
 
+        /// <summary>
+        /// PartiallyUpdateSoldPositionForCurrentPosition
+        /// </summary>
+        /// <param name="ticker"></param>
+        /// <param name="number"></param>
+        /// <param name="patchDocument"></param>
+        /// <returns></returns>
+        /// <response code="204">If the sold position is updated</response>
+        /// <response code="404">If the Current Position or sold position couldn't be found</response>
+        /// <response code="201">If the sold position is created</response>
+        //Patch api/currentPositions/xxx/soldPositions/1
+        [HttpPatch("{number}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult PartiallyUpdateSoldPositionForCurrentPosition(string ticker, long number, JsonPatchDocument<SoldPositionForUpdateDto> patchDocument)
+        {
+            if (!_currentPositionsRepository.CurrentPositionExists(ticker))
+            {
+                return NotFound($"No current position found for the ticker {ticker}.");
+            }
+
+            var soldPositionFromRepo = _soldPositionsRepository.GetSoldPosition(ticker, number);
+            if (soldPositionFromRepo == null)
+            {
+                var soldPositionForUpdateDto = new SoldPositionForUpdateDto();
+                patchDocument.ApplyTo(soldPositionForUpdateDto, ModelState);
+
+                if (!TryValidateModel(soldPositionForUpdateDto))
+                {
+                    return ValidationProblem(ModelState);
+                }
+
+                var soldPositionEntity = _mapper.Map<SoldPosition>(soldPositionForUpdateDto);
+                soldPositionEntity.Number = number;
+
+                _soldPositionsRepository.Add(ticker, soldPositionEntity);
+                _soldPositionsRepository.Save();
+
+                if (AddClosedPositionBasedOnTotalShares(ticker, soldPositionEntity))
+                    return NoContent();
+              
+                var soldPositionToReturn = _mapper.Map<SoldPositionDto>(soldPositionEntity);
+                return CreatedAtRoute("GetSoldPositionForCurrentPosition",
+                    new { ticker = ticker, number = soldPositionToReturn.Number },
+                    soldPositionToReturn);
+            }
+
+            var  soldPositionToPatch= _mapper.Map<SoldPositionForUpdateDto>(soldPositionFromRepo);
+            patchDocument.ApplyTo(soldPositionToPatch, ModelState);
+
+            if (!TryValidateModel(soldPositionToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(soldPositionToPatch, soldPositionFromRepo);
+
+            _soldPositionsRepository.Update(soldPositionFromRepo);
+            _soldPositionsRepository.Save();
+
+            if (AddClosedPositionBasedOnTotalShares(ticker, soldPositionFromRepo))
+                return NoContent();
+
+            return NoContent();
+        }
+
+        
         /// <summary>
         /// DeleteSoldPositionForCurrentPosition
         /// </summary>
@@ -335,6 +382,62 @@ namespace StockInvestments.API.Controllers
             }
             
             return NoContent();
+        }
+
+        private bool AddClosedPositionBasedOnTotalShares(string ticker, SoldPosition soldPositionEntity)
+        {
+            var currentPosition = _currentPositionsRepository.GetCurrentPosition(ticker);
+
+            var sharesRemaining = _soldPositionsRepository.GetSharesRemaining(currentPosition);
+
+            if (sharesRemaining == 0)
+            {
+                var totalAmount = _soldPositionsRepository.GetSoldPositionsTotalAmount(ticker);
+                var closedPosition = new ClosedPosition()
+                {
+                    Ticker = ticker,
+                    Company = currentPosition.Company,
+                    FinalValue = totalAmount - currentPosition.TotalAmount
+                };
+                _closedPositionsRepository.Add(closedPosition);
+                _soldPositionsRepository.Save();
+                _currentPositionsRepository.Delete(currentPosition);
+                _soldPositionsRepository.Save();
+
+                return true;
+            }
+
+            //var currentPosition = _currentPositionsRepository.GetCurrentPosition(ticker);
+
+            //var sharesRemaining = _soldPositionsRepository.GetSharesRemaining(currentPosition);
+
+            //if (sharesRemaining == 0)
+            //{
+            //    var totalAmount = _soldPositionsRepository.GetSoldPositionsTotalAmount(ticker);
+            //    var closedPosition = new ClosedPosition()
+            //    {
+            //        Ticker = ticker,
+            //        Company = currentPosition.Company,
+            //        FinalValue = (totalAmount) / currentPosition.TotalShares - currentPosition.TotalAmount
+
+            //    };
+            //    _closedPositionsRepository.Add(closedPosition);
+            //    _soldPositionsRepository.Save();
+            //    _currentPositionsRepository.Delete(currentPosition);
+            //    _soldPositionsRepository.Save();
+            //}
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelStateDictionary"></param>
+        /// <returns></returns>
+        public override ActionResult ValidationProblem([ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
+        {
+            var options = HttpContext.RequestServices.GetRequiredService<IOptions<ApiBehaviorOptions>>();
+            return (ActionResult)options.Value.InvalidModelStateResponseFactory(ControllerContext);
         }
     }
 }
