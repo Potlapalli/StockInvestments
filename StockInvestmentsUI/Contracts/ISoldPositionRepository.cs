9f1bff8414dfa26702674d2cb37a7037e6d4b6af
Ananth Rao <potlapalliananth@gmail.com>
2021-08-09 20:39:10 -0500

    Add files via upload

---
+++ StockInvestmentsUI/Contracts/ISoldPositionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StockInvestmentsUI.Models;
+
+namespace StockInvestmentsUI.Contracts
+{
+    public interface ISoldPositionRepository : IRepositoryBase<SoldPosition>
+    {
+        Task<IList<string>> GetPositionsInProfit(string url);
+       
+        Task<double> GetSharesRemaining(string url);
+    }
+}
---
+++ StockInvestmentsUI/Models/FilterCritieria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockInvestmentsUI.Models
+{
+    public class FilterCritieria
+    {
+        public double TotalAmountFilterValue { get; set; }
+
+        public string Ticker { get; set; }
+    }
+}
